68a557b095118a571316d7c2a67d421970a5b3d2
Martin Gontscharow <gontscharow@fzi.de>
2025-01-22 09:35:38 +0100

    change hearbeat type

--- ws/session/content/base/heartbeat.py
+++ ws/session/content/base/heartbeat.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Header as Heartbeat
+from geometry_msgs.msg import PoseStamped
 
 def publish_heartbeat():
-    rospy.init_node("heartbeat_publisher", anonymous=True)  # Initialisiere Node
+    rospy.init_node("heartbeat_publisher")  # Initialisiere Node
 
     # Topicname aus Parameter holen, Standardwert ist "/heartbeat_topic"
     topic_name = rospy.get_param("~topic_name", "/heartbeat_topic")
-    pub = rospy.Publisher(topic_name, Heartbeat, queue_size=10)  # Publisher erstellen
+    pub = rospy.Publisher(topic_name, PoseStamped, queue_size=10)  # Publisher erstellen
     rate = rospy.Rate(1)  # 1 Hz Frequenz
 
     seq = 0  # Sequenznummer starten
 
     while not rospy.is_shutdown():
-        heartbeat_msg = Heartbeat()
-        heartbeat_msg.seq = seq  # Setze Sequenznummer
-        heartbeat_msg.stamp = rospy.Time.now()  # ROS-Zeit
+        heartbeat_msg = PoseStamped()
+        heartbeat_msg.header.seq = seq  # Setze Sequenznummer
+        heartbeat_msg.header.stamp = rospy.Time.now()  # ROS-Zeit
 
-        rospy.loginfo(f"Publishing Heartbeat on {topic_name}: seq={seq}, stamp={heartbeat_msg.stamp.to_sec()}, frame_id={heartbeat_msg.frame_id}")
+        rospy.loginfo(f"Publishing Heartbeat on {topic_name}: seq={seq}, stamp={heartbeat_msg.header.stamp.to_sec()}")
         pub.publish(heartbeat_msg)  # Nachricht publizieren
 
         seq += 1  # Sequenznummer erhöhen
